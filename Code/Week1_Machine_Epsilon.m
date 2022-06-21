70ba72edee67af348ec54178ca0842aca362108c
bumsu-kim <49388481+bumsu-kim@users.noreply.github.com>
2022-06-21 14:39:37 -0700

    Week 1 After Discussion
    Fixed typos, Added comments

--- Code/Week1_Machine_Epsilon.m
+++ Code/Week1_Machine_Epsilon.m
@@ -1,16 +1,26 @@
 e = machine_eps(100); % run machine_eps function with max_iter = 100
 
+%% The Below is the Function Definition of machine_eps
+
+% Syntax: 
+% function [return_value] = [function_name] ( [parameters] )
 function eps = machine_eps(max_iter)
-eps = 1;
+eps = 1; % start from 1
+
+% for loop,
+% looping through i = 1, 2, 3, ... max_iter
+% (or until "break")
 for i = 1:max_iter
     if ( 1 + eps/2 == 1)
         break;
     else
         eps = eps / 2;
-    end
-end
+    end % indicates the end of if-else clause
+end     % indicates the end of the for loop
+
+% display output
 disp(['machine eps = ', num2str(eps)]);
 disp([ ' = 2^(-' , num2str(i-1), ')']);
 % or you can use "fprintf" if you prefer
-% fprintf('machine eps = %.4e = 2^(-%d)\n', eps, i-1);
+%fprintf('machine eps = %.4e = 2^(-%d)\n', eps, i-1);
 end
